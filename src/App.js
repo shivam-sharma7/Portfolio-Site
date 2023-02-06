d7fc5858f7c5e428112981ba2da2151673972ee1
Shivam Sharma <meshivam81@gmail.com>
2023-02-06 16:37:34 +0530

    added necessary files and deleted unecessary files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
+import About from './pages/About'
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Container>
+        <Router>
+          <Header/>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/services' element={<Services />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </Router>
+      </Container>
   );
 }
 
